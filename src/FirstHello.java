ee5536d976268503d6c23b2b1ac8febcde181709
dietaschuelter <dieta.schuelter@goeuro.com>
2016-04-16 10:05:27 +0200

    first checkin

---
+++ src/FirstHello.java
@@ -0,0 +1,13 @@
+/**
+ * Created by dietaschuelter on 4/16/16.
+ */
+public class FirstHello {
+
+//    public static void main(String[] args) {
+//      System.out.println("First Hello!");
+//
+//        int myHugo = 10;
+//        System.out.println(myHugo);
+//    }
+
+}
---
+++ src/TestString.java
@@ -0,0 +1,14 @@
+/**
+ * Created by dietaschuelter on 4/13/16.
+ */
+public class TestString {
+    public static void main(String[] args) {
+        int myInt = 7;
+        String myText = "grizzliwutz";
+
+        System.out.println(myText);
+
+
+    }
+
+}
